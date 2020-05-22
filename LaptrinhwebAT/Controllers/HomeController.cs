ecc343f4f8313152eb58fbeaca12a8d983862ff7
‘ivermin1123’ <mrkjin99@gmail.com>
2020-05-23 01:01:29 +0700

    first commit

---
+++ LaptrinhwebAT/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaptrinhwebAT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Index";
+            return View();
+        }
+        public ActionResult Nguyen()
+        {
+            ViewBag.Message = "Nguyen";
+            return View();
+        }
+
+        public ActionResult Tran()
+        {
+            ViewBag.Message = "Tran";
+
+            return View();
+        }
+
+        public ActionResult Anh()
+        {
+            ViewBag.Message = "Anh";
+
+            return View();
+        }
+
+        public ActionResult Tu()
+        {
+            ViewBag.Message = "Tu";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ LaptrinhwebAT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LaptrinhwebAT.MvcApplication" Language="C#" %>
